316f49e92d2cacbdf7df5c5e0c803fcfe14ac3aa
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-09-13 18:09:57 +0530

    some minor refactoring and added hls.js to index.html

--- lib/appwrite.dart
+++ lib/appwrite.dart
@@ -1,16 +1,35 @@
 import 'package:http/http.dart' as http;
 
 final class Appwrite {
-  static const serverEndpoint =
-      'https://cloud.appwrite.io/v1/databases/main_database/collections/c_id/documents';
+  static const serverEndpoint = 'https://cloud.appwrite.io/v1';
   static const headers = {
     'X-Appwrite-Project': projectId,
     'Content-Type': 'application/json',
   };
   static const projectId = '66b20ea5002316ed487f';
 
-  static getDoc() async {
-    final doc = await http.get(Uri.parse(serverEndpoint), headers: headers);
+  static Future<dynamic> listDocument({
+    String databaseId = "main_database",
+    required String collectionId,
+    required String docId,
+  }) async {
+    final doc = await http.get(
+      Uri.parse(
+          "$serverEndpoint/databases/$databaseId/collections/$collectionId/documents/$docId"),
+      headers: headers,
+    );
+    print(doc);
+  }
+
+  static Future<dynamic> listDocuments({
+    String databaseId = "main_database",
+    required String collectionId,
+  }) async {
+    final doc = await http.get(
+      Uri.parse(
+          "$serverEndpoint/databases/$databaseId/collections/$collectionId/documents"),
+      headers: headers,
+    );
     print(doc);
   }
 }
