b87cd7c7797e01842d509a01761831064322a945
Raman Tank <ramantank04022002@gmail.com>
2024-10-02 21:14:24 +0530

    Refactor splash menu component styles

--- lib/presentation/screens/splash/menu.dart
+++ lib/presentation/screens/splash/menu.dart
@@ -32,19 +32,20 @@ class MenuState extends State<Menu> {
         if (passengers == null) {
           yield img(
             src: 'images/spinner.gif',
-            classes: 'align-center',
-            styles: const Styles.combine(
-              [
-                Styles.box(
-                  width: Unit.vw(4),
-                  height: Unit.vw(4),
-                  position: Position.absolute(
-                    left: Unit.percent(50),
-                    bottom: Unit.pixels(20),
-                  ),
-                ),
-              ],
-            ),
+            // styles: const Styles.combine(
+            //   [
+            //     Styles.box(
+            //       width: Unit.vw(4),
+            //       height: Unit.vw(4),
+            //       position: Position.absolute(
+            //         left: Unit.percent(50),
+            //         bottom: Unit.pixels(20),
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            classes:
+                'align-center w-[4vw] h-[4vw] absolute left-1/2 bottom-[20px]',
           );
           return;
         }
@@ -56,34 +57,34 @@ class MenuState extends State<Menu> {
                   'Who\'s Watching?',
                 ),
               ],
-              classes: 'pb-3',
-              styles: const Styles.text(
-                align: TextAlign.center,
-                color: Colors.white,
-                fontSize: Unit.pixels(35),
-                fontWeight: FontWeight.bold,
-                wordSpacing: Unit.pixels(1),
-              ),
+              // styles: const Styles.text(
+              //   align: TextAlign.center,
+              //   color: Colors.white,
+              //   fontSize: Unit.pixels(35),
+              //   fontWeight: FontWeight.bold,
+              //   wordSpacing: Unit.pixels(1),
+              // ),
+              classes:
+                  'pb-3 text-center text-white text-[35px] font-bold tracking-[1px]',
             ),
-            div(
-              [
-                ...passengers.map(
-                  (passenger) {
-                    return Box(passenger.fName);
-                  },
-                )
-              ],
-              styles: const Styles.combine(
-                [
-                  Styles.flexbox(
-                    justifyContent: JustifyContent.center,
-                    alignItems: AlignItems.center,
-                    direction: FlexDirection.row,
-                    wrap: FlexWrap.wrap,
-                  ),
-                ],
-              ),
-            )
+            div([
+              ...passengers.map(
+                (passenger) {
+                  return Box(passenger.fName);
+                },
+              )
+            ],
+                // styles: const Styles.combine(
+                //   [
+                //     Styles.flexbox(
+                //       justifyContent: JustifyContent.center,
+                //       alignItems: AlignItems.center,
+                //       direction: FlexDirection.row,
+                //       wrap: FlexWrap.wrap,
+                //     ),
+                //   ],
+                // ),
+                classes: 'flex justify-center items-center flex-row flex-wrap')
           ],
           classes: 'bg-black bg-opacity-20 rounded-[20px]',
         );
@@ -100,7 +101,6 @@ class Box extends StatelessComponent {
 
   @override
   Iterable<Component> build(BuildContext context) sync* {
-    Unit commonUnit = const Unit.vh(8);
 
     yield FocusComponent(
       onTap: () {
@@ -114,52 +114,62 @@ class Box extends StatelessComponent {
           [
             h1(
               [text('${title[0]}${title[1]}'.toUpperCase())],
-              styles: Styles.combine(
-                [
-                  Styles.background(
-                      color: Color.rgb(
-                        random.nextInt(255),
-                        random.nextInt(255),
-                        random.nextInt(255),
-                      ),
-                      size: BackgroundSize.sides(
-                        commonUnit,
-                        commonUnit,
-                      )),
-                  Styles.text(
-                      align: TextAlign.center,
-                      lineHeight: commonUnit,
-                      fontSize: const Unit.pixels(30)),
-                  Styles.box(
-                    width: commonUnit,
-                    height: commonUnit,
-                    radius: const BorderRadius.circular(
-                      Unit.percent(50),
-                    ),
-                  ),
-                ],
-              ),
-              classes: 'border-4 border-white',
+              styles: Styles.combine([
+                Styles.background(
+                    color: Color.rgb(
+                  random.nextInt(255),
+                  random.nextInt(255),
+                  random.nextInt(255),
+                ))
+              ]),
+              // styles: Styles.combine(
+              //   [
+              //     Styles.background(
+              //         color: Color.rgb(
+              //           random.nextInt(255),
+              //           random.nextInt(255),
+              //           random.nextInt(255),
+              //         ),
+              //         size: BackgroundSize.sides(
+              //           commonUnit,
+              //           commonUnit,
+              //         )),
+              //     Styles.text(
+              //         align: TextAlign.center,
+              //         lineHeight: commonUnit,
+              //         fontSize: const Unit.pixels(30)),
+              //     Styles.box(
+              //       width: commonUnit,
+              //       height: commonUnit,
+              //       radius: const BorderRadius.circular(
+              //         Unit.percent(50),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              classes:
+                  'border-4 border-white w-[8vh] h-[8vh] bg-random text-center leading-[8vh] text-[30px] rounded-full',
             ),
             h3([text(title)]),
           ],
-          styles: const Styles.combine(
-            [
-              Styles.box(
-                radius: BorderRadius.all(
-                  Radius.circular(
-                    Unit.pixels(20),
-                  ),
-                ),
-                padding: EdgeInsets.all(Unit.pixels(20)),
-              ),
-              Styles.flexbox(
-                justifyContent: JustifyContent.center,
-                alignItems: AlignItems.center,
-                direction: FlexDirection.column,
-              ),
-            ],
-          ),
+          // styles: const Styles.combine(
+          //   [
+          //     Styles.box(
+          //       radius: BorderRadius.all(
+          //         Radius.circular(
+          //           Unit.pixels(20),
+          //         ),
+          //       ),
+          //       padding: EdgeInsets.all(Unit.pixels(20)),
+          //     ),
+          //     Styles.flexbox(
+          //       justifyContent: JustifyContent.center,
+          //       alignItems: AlignItems.center,
+          //       direction: FlexDirection.column,
+          //     ),
+          //   ],
+          // ),
+            classes: 'rounded-[20px] p-[20px] flex justify-center items-center flex-col',
         ),
       ],
     );
