a3cb951abcd0d458b7ca48634304fdbe09c653d1
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-10-02 22:48:40 +0530

    vod screen with hls

---
+++ lib/presentation/screens/VOD/hls_video_player.dart
@@ -0,0 +1,63 @@
+import 'dart:js_interop';
+
+import 'package:jaspr/jaspr.dart';
+import 'package:web/web.dart';
+
+class HLSVideoComponent extends StatefulComponent {
+  final String streamUrl;
+  const HLSVideoComponent({super.key, required this.streamUrl});
+
+  @override
+  State<HLSVideoComponent> createState() => _HLSVideoComponentState();
+}
+
+class _HLSVideoComponentState extends State<HLSVideoComponent> {
+  @override
+  void initState() {
+    try {
+      final element = document.body;
+      element?.requestFullscreen();
+    } catch (e) {
+      console.error('Error going fullscreen: $e'.toJS);
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    try {
+      // removing video
+      final player = document.getElementById('hls-example');
+      player?.remove();
+    } catch (e) {
+      console.error('Error stopping player: $e'.toJS);
+    }
+    super.dispose();
+  }
+
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    yield section([
+      video(
+        [
+          source(
+            src: component.streamUrl,
+            type: 'application/x-mpegURL',
+          )
+        ],
+        autoplay: true,
+        id: 'hls-example',
+        classes:
+            'video-js vjs-default-skin position-absolute top-0 left-0 w-full h-full',
+        controls: false,
+      ),
+      script([], src: 'https://cdn.jsdelivr.net/npm/hls.js@latest'),
+      raw("""
+      <script>
+        var player = videojs('hls-example');
+        player.play();
+      </script>
+      """),
+    ]);
+  }
+}
--- lib/presentation/screens/VOD/video_on_demand.dart
+++ lib/presentation/screens/VOD/video_on_demand.dart
@@ -1,5 +1,8 @@
 import 'package:jaspr/jaspr.dart';
+import 'package:jaspr_router/jaspr_router.dart';
 
+import '../../../app/routes.dart';
+import '../../../core/utils/focus_component.dart';
 import '../../../data/models/movie/movie.dart';
 import 'video_on_demand_model.dart';
 
@@ -10,32 +13,54 @@ class VideoOnDemand extends StatelessComponent {
   Iterable<Component> build(BuildContext context) sync* {
     final videoOnDemandModel = VideoOnDemandModel();
 
-    yield div([
-      div(
-        [
-          ...Movie.allMovies.map((movie) {
-            return div([
-              div(
-                [
-                  h2([text(movie.title)],
-                      classes: 'text-white text-lg font-bold'),
-                ],
-                classes:
-                    'absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center',
-              ),
-            ],
-                classes:
-                    'relative rounded-lg overflow-hidden bg-cover bg-center h-64',
-                styles: Styles.background(
-                  image: ImageStyle.url(
-                    movie.image,
+    yield div(
+      [
+        ...Movie.allMovies.map(
+          (movie) {
+            return FocusComponent(
+              children: [
+                div(
+                  [
+                    div(
+                      [
+                        h2([text(movie.title)],
+                            classes: 'text-white text-lg font-bold'),
+                      ],
+                      classes:
+                          'absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center',
+                    ),
+                  ],
+                  classes:
+                      'relative rounded-40 overflow-hidden bg-cover bg-center h-64',
+                  styles: Styles.combine(
+                    [
+                      const Styles.box(
+                        radius: BorderRadius.circular(Unit.pixels(20)),
+                      ),
+                      Styles.background(
+                        image: ImageStyle.url(movie.image),
+                      ),
+                    ],
                   ),
-                ));
-          }),
-        ],
-      ),
-    ],
-        classes:
-            'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-${videoOnDemandModel.numColumns} gap-4 justify-center');
+                ),
+              ],
+              onTap: () {
+                Router.of(context).push(
+                  AppRoutes.hls,
+                  extra: movie.streamUrl,
+                );
+              },
+            );
+          },
+        ),
+      ],
+      styles: Styles.grid(
+          gap: const GridGap.all(Unit.pixels(2)),
+          template: GridTemplate(
+              columns: GridTracks([
+            for (var i = 0; i < videoOnDemandModel.numColumns; i++)
+              const GridTrack(TrackSize.fr(1)),
+          ]))),
+    );
   }
 }
